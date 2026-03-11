084de2639454082e4c412416b2e56d27302fed61
chaim <1099732@hr.nl>
2026-03-11 13:56:00 +0100

    login redirect nu als je of admin bent naar admin dash of gebruiker naar gebruiker dash

--- TLE3/src/components/Navbar.jsx
+++ TLE3/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ export default function Navbar() {
                         <Link to="/artikel" className={blackBtnStyle}><PlusSquare size={20} /><span>Artikel</span></Link>
                         <Link to="/ai-transparantie" className={blackBtnStyle}><Cpu size={20} /><span>AI</span></Link>
                         <Link to="/settings" className={blackBtnStyle}><Settings size={20} /><span>Instellingen</span></Link>
-                        <Link to="/loguit" className={blackBtnStyle}><LogOut size={20} /><span>Uitloggen</span></Link>
+                        <Link to="/logout" className={blackBtnStyle}><LogOut size={20} /><span>Uitloggen</span></Link>
                     </div>
 
                     {/* MOBILE BUTTON */}
